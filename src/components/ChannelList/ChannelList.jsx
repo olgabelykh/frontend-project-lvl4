bd20d9704b3c7adf4c3d7b5c6ca8f25747aa638a
Olya Belykh <belykh.o.v@gmail.com>
2020-12-12 19:11:25 +0300

    linter bug fixed

--- src/components/ChannelList/ChannelList.jsx
+++ src/components/ChannelList/ChannelList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const ChannelList = ({ channels = [] }) => {
   const renderChannels = () =>
+    // eslint-disable-next-line implicit-arrow-linebreak
     channels.map(({ id, name }) => (
       <li key={id} className="list-group-item">
         {name}
